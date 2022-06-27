77b231e8f4577820cd87832a2b9e024374d48b28
Vaibhav Gangrade <45598521+Viv31@users.noreply.github.com>
2022-06-27 22:56:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-//         <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+import logo from './logo.svg';
+import './App.css';
+ import React, { Component } from 'react';
+ import Header from './components/Header.jsx';
+ import Aboutme from './components/Aboutme.jsx';
+ import Experience from './components/Experience.jsx';
+ import Education from './components/Education.jsx';
+ import Skills from './components/Skills.jsx';
+ import Footer from './components/Footer.jsx';
+ import Achievements from './components/Achievements.jsx';
+function App() {
+  return (
+    <>
+   <Header></Header>
+   <Aboutme></Aboutme>
+  <Experience></Experience>
+  <Education></Education>
+  <Skills></Skills>
+  <Achievements></Achievements>
+  <Footer></Footer>
+  </>
+);
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
